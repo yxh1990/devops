c701e1092df9c119f34b81608ed40c660fd7fd65
yxh1990 <1272724627@qq.com>
2020-06-27 16:16:16 +0800

    Update gitlab.groovy

--- src/org/devops/gitlab.groovy
+++ src/org/devops/gitlab.groovy
@@ -3,7 +3,7 @@ package org.devops
 //封装HTTP请求
 def HttpReq(reqType,reqUrl,reqBody){
     def gitServer = "http://192.168.19.138:8090/api/v4"
-    withCredentials([string(credentialsId: 'gitlab-token', variable: 'gitlabToken')]) {
+    withCredentials([string(credentialsId: 'gitlab', variable: 'gitlabToken')]) {
       result = httpRequest customHeaders: [[maskValue: true, name: 'PRIVATE-TOKEN', value: "${gitlabToken}"]], 
                 httpMode: reqType, 
                 contentType: "APPLICATION_JSON",
